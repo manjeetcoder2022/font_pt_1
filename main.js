eb66796aa1739b89ece6511595fc11ec2425b556
manjeetcoder2022 <99887565+manjeetcoder2022@users.noreply.github.com>
2022-12-24 14:05:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas= createCanvas(400,400)
+    background("pink")
+    video= createCapture(VIDEO)
+    video.size(400,400)
+    canvas.center()
+}
+function draw(){
+
+}
